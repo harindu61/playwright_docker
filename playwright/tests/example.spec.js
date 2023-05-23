a8d7ce69dde3651c3c6986ff97c848253b2a0137
Hari <harindu61@gmail.com>
2023-05-24 00:52:33 +0700

    Update example.spec.js

--- playwright/tests/example.spec.js
+++ playwright/tests/example.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('http://host.docker.internal:3000');
+  await page.goto('http://172.17.0.1:3000');
 
   // Expect a title "to contain" a substring.
   await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
