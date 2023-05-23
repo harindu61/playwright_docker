2d71af146eb7e1778dc19ee3bf350429bf7acb7d
Hari <harindu61@gmail.com>
2023-05-23 23:16:45 +0700

    Update example.spec.js

--- playwright/tests/example.spec.js
+++ playwright/tests/example.spec.js
@@ -1,10 +1,19 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has body test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
