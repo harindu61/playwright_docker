750c61ff09b49f4ffb1ae8dcb7150963e3084067
Hari <harindu61@gmail.com>
2023-06-01 13:58:29 +0700

    Update playwright.config.js

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -12,6 +12,7 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
+
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -36,7 +37,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
+    video: 'retain-on-failure',
     screenshot: 'only-on-failure',
   },
 
