8b29a3343f46976ed00afc199fa38a452181dfba
Hari <harindu61@gmail.com>
2023-06-01 15:24:27 +0700

    Update example2.spec.js

--- playwright/tests/example2.spec.js
+++ playwright/tests/example2.spec.js
@@ -1,8 +1,23 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+async function getConsoleErrorLog(page) {
+  page.on('console', async (msg) => {
+    console.log(`\nPAGE: ${page.url()}`)
+    console.log(`ERROR: ${msg.text()}`)
+    console.log(`Location: ${msg.location().url}`)
+
+    if (msg.args().length > 0) {
+      console.log('ARG:')
+      console.log(msg.args())
+    }
+    console.log('------------------------------')
+  })
+}
+
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  getConsoleErrorLog(page)
+  await page.goto('https://www.google.com/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get estarted' }).click();
