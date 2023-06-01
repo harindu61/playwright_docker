c23b874fa62dd8332ba2529d95277e282b82b9ed
Hari <harindu61@gmail.com>
2023-06-01 13:53:24 +0700

    Update example2.spec.js

--- playwright/tests/example2.spec.js
+++ playwright/tests/example2.spec.js
@@ -5,7 +5,7 @@ test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Get estarted' }).click();
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
