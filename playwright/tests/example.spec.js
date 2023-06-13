02ffe199d2c7fec849a1db63801f88a73427ec65
Hari <harindu61@gmail.com>
2023-06-13 08:38:45 +0700

    Update example.spec.js

--- playwright/tests/example.spec.js
+++ playwright/tests/example.spec.js
@@ -1,7 +1,70 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
+test('has content 01', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
+});
+
+test('has content 02', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
+});
+
+test('has content 03', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
+});
+
+test('has content 04', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
+});
+
+test('has content 05', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
+});
+
+test('has content 06', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
+});
+
+test('has content 07', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
+});
+
+test('has content 08', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
+});
+
+test('has content 09', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByText('Hello from inside the very basic Node app!')).toBeVisible();
+});
+
+test('has content 10', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   // Expect a title "to contain" a substring.
