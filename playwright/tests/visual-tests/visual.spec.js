aca2312fec1d05b90ad876ca7ff0ed8bb01fa235
Hari <harindu61@gmail.com>
2023-06-13 08:17:49 +0700

    Update visual.spec.js

--- playwright/tests/visual-tests/visual.spec.js
+++ playwright/tests/visual-tests/visual.spec.js
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('playwright web test', async ({ page }) => {
+test.skip('playwright web test', async ({ page }) => {
   await page.goto('https://playwright.dev');
   await expect(page).toHaveScreenshot();
 });
 
-test('facebook web test', async ({ page }) => {
+test.skip('facebook web test', async ({ page }) => {
   await page.goto('https://www.facebook.com');
   await expect(page).toHaveScreenshot();
 });
